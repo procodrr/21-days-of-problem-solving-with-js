0a16774d976d0ea1731c440a78f1dc2bfa325d43
Faizan Khan <faizannkhan72@gmail.com>
2026-02-14 14:55:10 +0530

    day 1 solution 2 added

--- Day-1_simple-number-problems/my-solution.js
+++ Day-1_simple-number-problems/my-solution.js
@@ -1,7 +1,18 @@
 // 1. **Print Numbers from 1 to N**
+console.log("1. Print Numbers from 1 to N");
 
 const num = 10;
 
 for (let i = 1; i <= num; i++) {
+  console.log(i);
+}
+
+//2. **Print Numbers from N to 1 without changing the loop condition of above question**
+console.log(
+  "2. Print Numbers from N to 1 without changing the loop condition of above question"
+);
+
+let inpt = 5;
+for (let i = inpt; i >= 1; i--) {
   console.log(i);
 }
